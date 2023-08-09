d75eed31dd45c8c86c157a6f2b47d0415d486dea
MihalMadasseriMoideen <141790685+MihalMadasseriMoideen@users.noreply.github.com>
2023-08-09 02:37:41 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,33 @@
+window.reviewData = [
+    {
+      name: "Sarah",
+      date: "2023-07-01",
+      rating: 3,
+      review: "It was nice, but I failed the match and so I am not satisfied.."
+    },
+    {
+      name: "James",
+      date: "2023-07-20",
+      rating: 5,
+      review: "I enjoyed this game. It made me happy."
+    },
+    {
+      name: "Lahen",
+      date: "2023-06-03",
+      rating: 2,
+      review: "It's okay, but I feel tired.."
+    },
+    {
+      name: "Tim Hook",
+      date: "2023-05-15",
+      rating: 4,
+      review: "I like the game. Will practice later."
+    },
+    {
+      name: "Brit Heek",
+      date: "2023-03-15",
+      rating: 5,
+      review: "My favourite game, I enjoyed a lot."
+    }
+  ];
+  
